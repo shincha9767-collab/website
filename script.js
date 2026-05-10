760425213fd6b48556725c6bd75e72006c4fe5f2
shincha9767-collab <shincha.9767@gmail.com>
2026-05-11 00:59:24 +0530

    3

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
+const music = document.getElementById("music");
+
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
 
-    alert("Welcome ❤️");
-
-});
-const music = document.getElementById("music");
-
-document.body.addEventListener("click", () => {
-
     music.play();
-
-}, { once:true });
-
-const button = document.querySelector("button");
-
-button.addEventListener("mouseenter", () => {
-
-    button.innerHTML = "Open It ❤️";
-
-});
-
-button.addEventListener("mouseleave", () => {
-
-    button.innerHTML = "Enter 💌";
 
 });
